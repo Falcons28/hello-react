dc9757bc119c05c2ce50ab8ed5c3ff7f4c81edb6
Falcons28 <gabrielfloresgaragundo.1999@gmail.com>
2024-08-16 01:54:26 +0000

    agregamos el componente ciudad con sus respectivos codigos

--- Gab_288/src/App.jsx
+++ Gab_288/src/App.jsx
@@ -1,12 +1,19 @@
 import { Fragment } from "react";
 import Titulo from "./componentes/Titulo";
 import Pie from "./componentes/Pie";
+import Ciudad from "./componentes/Ciudad"
+
 function App () {
 
   return (
 
     <>
     <Titulo/>
+    <Ciudad
+    nombre ="Lima"
+    descripcion ="Capital del Perú"
+    imagen ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB_AXhjaPJMdz6lVd83xzinN20U-tNEQc-jg&s"
+    />
     <Pie/>
     </>
   );
