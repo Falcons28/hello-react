017c32735235636d0ccab38915198bda8d4fc90f
Falcons28 <gabrielfloresgaragundo.1999@gmail.com>
2024-08-16 01:15:17 +0000

    He agregado un logo

---
+++ Gab_288/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ciberteclogo from './image/dc.png'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.cibertec.edu.pe/" target="_blank">
+          <img src={ciberteclogo} className="logo" alt="Vite logo" />
+        </a> 
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click en los logos de vite  y react para saber más 
+      </p>
+    </>
+  )
+}
+
+export default App
