a8ec4203da8cb7652e9bfe4bb2c574978436c9d4
Falcons28 <gabrielfloresgaragundo.1999@gmail.com>
2024-08-16 01:19:40 +0000

    he creado el componente titulo

---
+++ Gab_288/src/componentes/Titulo.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function Titulo (){
+    return (
+        <>
+        <h1>CONOCE EL PERÚ PRIMERO CAUSA</h1>
+        <h2>EN EL PERÚ EXISTEN HERMOSOS LUGARES PARA CONOCER Y DISFRUTAR</h2>
+        </>
+    );
+}
+
+export default Titulo;
